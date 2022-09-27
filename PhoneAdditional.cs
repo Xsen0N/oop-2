ef30dc9a3334f6869f8c521ce38b7a18d15364ad
Ksenia Zhuk <kseni.zhyk.3@gmail.com>
2022-09-27 16:32:57 +0300

    Создание частичного класса, переопределение методов класса Object

---
+++ PhoneAdditional.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    public partial class  Phone
+    {
+        public override string ToString()
+        {
+            return "Person: " + "ID: " + ID + " " + "Имя: " + name + " Фамилия: "  + surname + " Отчество:  " + patronymic
+                + " Адрес: " + addres + " Номер карты:*" + cardnum.Substring(12, 4) +
+                " Время междугородных разговоров: " + timeoftalk + "мин Время  городских разговоров: " + timeoftalkcity + "мин Кредит " + kredit + " Дебет " + debit;
+        }
+        public override bool Equals(object obj)
+        {
+            // если параметр метода представляет тип Phone
+            // то возвращаем true, если фамилии совпадают
+            if (obj is Phone phone) return surname == phone.surname;
+            return false;
+        }
+        // вместе с методом Equals следует реализовать метод GetHashCode
+        public override int GetHashCode() => surname.GetHashCode();
+    }
+}
